444fe9bf9bca4cc88a6c2b692c0d9204c581d733
Rohith19 <rohith1512a@gmail.com>
2023-03-28 20:39:27 +0530

    Edited

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
-    const now = new Date();
+  const now = new Date();
 
-    const seconds = now.getSeconds();
-    const secondsDegree = ((seconds/60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+  const seconds = now.getSeconds();
+  const secondsDegree = ((seconds/60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegree}deg)`;
 
-    const mins = now.getMinutes();
-      const minsDegree = ((mins/60) * 360) + 90;
-    minsHand.style.transform = `rotate(${minsDegree}deg)`;
+  const mins = now.getMinutes();
+  const minsDegree = ((mins/60) * 360) + 90;
+  minsHand.style.transform = `rotate(${minsDegree}deg)`;
 
-    const hours = now.getHours();
-      const hoursDegree = ((hours/12) * 360) + 90;
-    hourHand.style.transform = `rotate(${hoursDegree}deg)`;
+  const hours = now.getHours();
+  const hourDegrees = ((hours / 12) * 360) + ((mins/60)*30) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 setInterval(setDate, 1000);
 setDate();
